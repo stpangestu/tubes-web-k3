6a4d95da178e675f9d254d74cbdb184f885087ca
stpangestu <saktitripangestu3291@gmail.com>
2022-01-05 17:09:28 +0700

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        return view('landing_page');
+    }
+    public function home()
+    {
+        return view('home');
+    }
+}
